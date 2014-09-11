c2d4d201b30bbb104933545b427b8987fc5bb815
cl4nk <kakemarb@gmail.com>
2014-09-11 18:39:00 +0200

    First commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	[HideInInspector]
+	public int curScore; 
+	// The highest score the player has reached (saved)
+	private int highscore;
+	
+	[HideInInspector]
+	public bool showGameOver;
+
+	// Use this for initialization
+	void Start () {
+		highscore = PlayerPrefs.GetInt("Highscore");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// If the bird died and our current score is greater than our saved highscore
+		if (showGameOver && curScore > highscore)
+		{
+			// Set the highscore to our current score
+			highscore = curScore;
+			// Now save the score as our new highscore
+			PlayerPrefs.SetInt("Highscore", highscore);
+		}
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public bool isDead = false;
+	private GameManager manager;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isDead) {
+			Die ();
+		} else {
+		
+		}
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		if (other.relativeVelocity.magnitude > 2) {
+			isDead = true;
+			Die();
+			}
+	}
+	
+	void Die()
+	{
+		manager.showGameOver = true;
+
+	}
+}
