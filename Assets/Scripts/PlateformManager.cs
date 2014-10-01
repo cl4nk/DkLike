8b88b606e0e1c62e92121116c9647bda7fe7365f
cl4nk <kakemarb@gmail.com>
2014-10-01 23:07:00 +0200

    Plateform Manager
    Marche pas du tout, mais au moins j'ai une base

--- Assets/Scripts/PlateformManager.cs
+++ Assets/Scripts/PlateformManager.cs
@@ -1,24 +1,54 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
-public class PlateformManager : MonoBehaviour {
-
+public class PlatformManager : MonoBehaviour {
+	
+	public Transform prefab;
 	public int numberOfObjects;
 	public float recycleOffset;
 	public Vector2 startPosition;
+	public Vector2 minSize, maxSize, minGap, maxGap;
 	public float minY, maxY;
 	
-	private Vector3 nextPosition;
+	private Vector2 nextPosition;
 	private Queue<Transform> objectQueue;
-
-	// Use this for initialization
-	void Start () {
-		nextPosition = startPosition;
 	
+	void Start () {
+		objectQueue = new Queue<Transform>(numberOfObjects);
+		for(int i = 0; i < numberOfObjects; i++){
+			objectQueue.Enqueue((Transform)Instantiate(prefab));
+		}
+		nextPosition = startPosition;
+		for(int i = 0; i < numberOfObjects; i++){
+			Recycle();
+		}
 	}
 	
-	// Update is called once per frame
 	void Update () {
+		if(objectQueue.Peek().localPosition.x + recycleOffset < Player.distanceTraveled){
+			Recycle();
+		}
+	}
 	
+	private void Recycle () {
+
+		Vector2 position = nextPosition;
+		position.x += scale.x * 0.5f;
+		position.y += scale.y * 0.5f;
+		
+		Transform o = objectQueue.Dequeue();
+		o.localPosition = position;
+		objectQueue.Enqueue(o);
+		
+		nextPosition += new Vector2(
+			Random.Range(minGap.x, maxGap.x) + scale.x,
+			Random.Range(minGap.y, maxGap.y));
+		
+		if(nextPosition.y < minY){
+			nextPosition.y = minY + maxGap.y;
+		}
+		else if(nextPosition.y > maxY){
+			nextPosition.y = maxY - maxGap.y;
+		}
 	}
 }
