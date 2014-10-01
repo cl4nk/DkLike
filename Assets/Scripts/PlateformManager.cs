a5d0a55a216c7644d953a52f269704887f5f1859
cl4nk <kakemarb@gmail.com>
2014-10-01 21:43:04 +0200

    Déplacement du script ScrollBackground

---
+++ Assets/Scripts/PlateformManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlateformManager : MonoBehaviour {
+
+	public int numberOfObjects;
+	public float recycleOffset;
+	public Vector2 startPosition;
+	public float minY, maxY;
+	
+	private Vector3 nextPosition;
+	private Queue<Transform> objectQueue;
+
+	// Use this for initialization
+	void Start () {
+		nextPosition = startPosition;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
